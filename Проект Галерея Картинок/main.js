3fff8757b42d8cdf31c8d5f1711f1217f6113ca5
Vlad <15vlad_yanko08@bk.ru>
2024-11-28 22:14:51 +0300

    finish

--- Проект Галерея Картинок/main.js
+++ Проект Галерея Картинок/main.js
@@ -4,28 +4,28 @@ function loadImages() {
     const loader = document.getElementById('loader');
     const gallery = document.getElementById('gallery');
     
-    // Показываем лоадер
+
     loader.style.display = 'block';
     
-    // Очистка галереи: удаляем все дочерние элементы
+ 
     while (gallery.firstChild) {
         gallery.removeChild(gallery.firstChild);
     }
 
-    // Отправляем запрос на сервер
+
     fetch('https://dog.ceo/api/breeds/image/random/20')
         .then(response => response.json())
         .then(data => {
-            // Скрываем лоадер, когда изображения загружены
+         
             loader.style.display = 'none';
             
-            // Заполняем галерею изображениями
+      
             const images = data.message;
             images.forEach(imageUrl => {
                 const img = document.createElement('img');
                 img.src = imageUrl;
                 img.alt = 'Dog image';
-                gallery.appendChild(img); // Добавляем новое изображение в галерею
+                gallery.appendChild(img); 
             });
         })
         .catch(error => {
