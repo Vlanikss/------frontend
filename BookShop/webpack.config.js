a22c3f60b680805905a7a820c8b93be5fea76841
Vlad <15vlad_yanko08@bk.ru>
2025-01-16 14:21:00 +0300

    add npm

---
+++ BookShop/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development', // Режим разработки
+  entry: './src/index.js', // Входной файл
+  output: {
+    filename: 'bundle.js', // Название итогового JS файла
+    path: path.resolve(__dirname, 'dist'), // Папка для сборки
+  },
+  devtool: 'source-map', // Генерация карт исходников для отладки
+  devServer: {
+    static: path.join(__dirname, 'dist'), // Папка с файлами статики
+    port: 9000, // Порт для сервера
+    hot: true, // Включить горячую перезагрузку
+    open: true, // Открывать браузер автоматически
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html', // Ваш HTML-шаблон
+      filename: 'index.html', // Название выходного файла
+    }),
+  ],
+  module: {
+    rules: [
+      // Правило для обработки CSS
+      {
+        test: /\.css$/i, // Для файлов с расширением .css
+        use: [
+          'style-loader', // Вставка CSS в DOM через <style> теги
+          'css-loader',   // Обработка CSS файлов
+        ],
+      },
+      // Правило для обработки SCSS
+      {
+        test: /\.scss$/i, // Для файлов с расширением .scss
+        use: [
+          'style-loader', // Вставка CSS в DOM через <style> теги
+          'css-loader',   // Обработка CSS файлов
+          'sass-loader',  // Компиляция SCSS в CSS
+        ],
+      },
+    ],
+  },
+};
