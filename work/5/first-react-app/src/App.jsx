dcf713b5fc24ce75bd70079d58ad595a07466c0b
Vlad <15vlad_yanko08@bk.ru>
2025-02-04 15:42:22 +0300

    .

---
+++ work/5/first-react-app/src/App.jsx
@@ -0,0 +1,20 @@
+import ParamEditor from "./ParamEditor";
+
+const params = [
+  { id: 1, name: "Назначение", type: "string" },
+  { id: 2, name: "Длина", type: "string" },
+];
+
+const model = {
+  paramValues: [
+    { paramId: 1, value: "повседневное" },
+    { paramId: 2, value: "макси" },
+  ],
+  colors: [],
+};
+
+function App() {
+  return <ParamEditor params={params} model={model} />;
+}
+
+export default App;
